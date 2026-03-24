0ca60f0480e2b32a820b9ab1115e242994a1be44
Manhnk2 <manhtt09@gmail.com>
2026-03-24 22:41:59 +0700

    feat (backend) : apply-security-to-system-setting-mgmt
    - Apply security to verify token automatically to protect API of service
     + add dependency oauth2-resourcer-server
     + config securirty

---
+++ backend/atpone-be/system-setting-mgtm/src/main/java/com/atpone/system/setting/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.atpone.system.setting.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Value("${url.public.auth-server}")
+	String publicAuthServerURL;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.cors(cors->cors.disable())
+				.csrf(csrf->csrf.disable())
+				.authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+				.oauth2ResourceServer(resourceServer->resourceServer.jwt(Customizer.withDefaults()))
+				.build();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withJwkSetUri(publicAuthServerURL.concat("/oauth2/jwks")).build();
+	}
+}
